c09fa53b09e22db449ad02fc77be6b7518c852c0
Jim DeLois <jim@deloisweb.com>
2019-02-25 10:17:39 -0500

    Adding Simple Header Row

--- bin/commands/stacks/list.js
+++ bin/commands/stacks/list.js
@@ -2,7 +2,7 @@ const { getConfig } = require('../../../lib/services/configuration');
 const sprintf = require('sprintf-js').sprintf;
 
 const nameLen = 20;
-let output = '';
+let output = [];
 
 module.exports = {
     command: 'list',
@@ -12,15 +12,23 @@ module.exports = {
     handler: async (argv) => {
         const config = await getConfig();
         const stacks = config.stacks;
+
+        output.push(sprintf(
+            `%-${nameLen}s %-36s %4s`,
+            'NAME',
+            'ID',
+            'APPS'
+        ));
+
         Object.keys(stacks).forEach((stackId) => {
-            output += sprintf(
-                `%-${nameLen}s %36s %4d Applications\n`,
+            output.push(sprintf(
+                `%-${nameLen}s %36s %4d`,
                 stacks[stackId].name.slice(0, nameLen),
                 stackId,
                 stacks[stackId].applications.length
-            );
+            ));
         });
 
-        process.stdout.write(output);
+        process.stdout.write(output.join('\n'));
     }
 };
