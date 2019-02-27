a91582036760dbef481688d47bb2daac0ad65fd8
Jim DeLois <jim@deloisweb.com>
2019-02-27 18:08:30 -0500

    Quick note on Command-specific Module Middleware

--- bin/commands/stacks.js
+++ bin/commands/stacks.js
@@ -4,6 +4,7 @@ module.exports = {
     aliases: ['ss'],
     desc: 'Interact with all Stacks collectively',
     builder: (yargs) => {
+        // One can yargs.middleware(middleWareFunc); here
         return yargs.commandDir('./stacks');
     },
     handler: (argv) => {}
